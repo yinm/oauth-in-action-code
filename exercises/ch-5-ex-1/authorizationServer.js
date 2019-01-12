03ec682e872553597dd8b64407ac885cee3d8027
yinm <yinm.cminor9@gmail.com>
2019-01-12 14:12:10 +0900

    Issue access tokens

--- exercises/ch-5-ex-1/authorizationServer.js
+++ exercises/ch-5-ex-1/authorizationServer.js
@@ -54,6 +54,8 @@ app.get("/authorize", function(req, res){
 	} else if (!__.contains(client.redirect_uris, req.query.redirect_uri)) {
 		res.render('error', { error: 'Invalid redirect URI' })
 	} else {
+		console.log('/authorize: ', req.query)
+
 		var reqid = randomstring.generate(8)
 		requests[reqid] = req.query
 
@@ -74,6 +76,8 @@ app.post('/approve', function(req, res) {
 
 	if (req.body.approve) {
 		if (query.response_type === 'code') {
+			console.log('/approve: ', query)
+
 			var code = randomstring.generate(8)
 
 			codes[code] = { request: query }
@@ -101,11 +105,61 @@ app.post('/approve', function(req, res) {
 });
 
 app.post("/token", function(req, res){
+	var auth = req.headers['authorization']
+	if (auth) {
+		var clientCredentials = decodeClientCredentials(auth)
+		var clientId = clientCredentials.id
+		var clientSecret = clientCredentials.secret
+	}
 
-	/*
-	 * Process the request, issue an access token
-	 */
+	if (req.body.client_id) {
+		if (clientId) {
+			res.status(401).json({ error: 'invalid_client' })
+			return
+		}
+		var clientId = req.body.client_id
+		var clientSecret = req.body.client_secret
+	}
 
+	var client = getClient(clientId)
+	if (!client) {
+		res.status(401).json({ error: 'invalid_client' })
+		return
+	}
+
+	if (client.client_secret !== clientSecret) {
+		res.status(401).json({ error: 'invalid_client' })
+		return
+	}
+
+	if (req.body.grant_type === 'authorization_code') {
+		var code = codes[req.body.code]
+
+		if (code) {
+			delete codes[req.body.code]
+
+			if (code.request.client_id === clientId) {
+				var access_token = randomstring.generate()
+				nosql.insert({ access_token: access_token, client_id: clientId })
+
+				console.log('Issuing access token %s', access_token)
+
+				var token_response = { access_token: access_token, token_type: 'Bearer' }
+				res.status(200).json(token_response)
+
+        return
+			} else {
+				res.status(400).json({ error: 'invalid_grant' })
+				return
+			}
+		} else {
+			res.status(400).json({ error: 'invalid_grant' })
+			return
+		}
+	} else {
+		res.status(400).json({ error: 'unsupported_grant_type' })
+		return
+	}
 });
 
 var buildUrl = function(base, options, hash) {

d9e02aae3cdf9b3420712f2c6696485964602e9e
yinm <yinm.cminor9@gmail.com>
2019-01-12 11:28:16 +0900

    Add different users for different data results

--- exercises/ch-4-ex-4/protectedResource.js
+++ exercises/ch-4-ex-4/protectedResource.js
@@ -32,11 +32,11 @@ var getAccessToken = function(req, res, next) {
 	} else if (req.query && req.query.access_token) {
 		inToken = req.query.access_token
 	}
-	
+
 	console.log('Incoming token: %s', inToken);
 	nosql.one(function(token) {
 		if (token.access_token == inToken) {
-			return token;	
+			return token;
 		}
 	}, function(err, token) {
 		if (token) {
@@ -71,14 +71,14 @@ var bobFavorites = {
 };
 
 app.get('/favorites', getAccessToken, requireAccessToken, function(req, res) {
-	
-	/*
-	 * Get different user information based on the information of who approved the token
-	 */
-	
-	var unknown = {user: 'Unknown', favorites: {movies: [], foods: [], music: []}};
-	res.json(unknown);
-
+  if (req.access_token.user === 'alice') {
+    res.json({user: 'Alice', favorites: aliceFavorites})
+	} else if (req.access_token.user === 'bob') {
+  	res.json({user: 'Bob', favorites: bobFavorites})
+	} else {
+		var unknown = {user: 'Unknown', favorites: {movies: [], foods: [], music: []}};
+		res.json(unknown);
+	}
 });
 
 var server = app.listen(9002, 'localhost', function () {
@@ -87,4 +87,4 @@ var server = app.listen(9002, 'localhost', function () {
 
   console.log('OAuth Resource Server is listening at http://%s:%s', host, port);
 });
- 
+
